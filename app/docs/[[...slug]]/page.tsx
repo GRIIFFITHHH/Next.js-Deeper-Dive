53a8832cadf4a2165291e3ee70ca0fa352d243d8
GRIIFFITHHH <makaluis9@gmail.com>
2025-09-22 15:58:46 +0200

    Update

---
+++ app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,14 @@
+export default function Docs({ params }: { params?: { slug?: string[] } }) {
+  return (
+    <div>
+      <h1>Docs</h1>
+      <ul>
+        {params?.slug?.length ? (
+          params.slug.map((s, i) => <li key={i}>{s}</li>)
+        ) : (
+          <li>Overview</li>
+        )}
+      </ul>
+    </div>
+  );
+}
